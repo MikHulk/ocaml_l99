ecacb9813d8dc7df36776bd125806b7e67060c29
MV <m.viey@wanadoo.fr>
2024-03-14 13:47:44 +0100

    05: reverse a list

--- 05_reverse_and_07_flatten_a_list/res.ml
+++ 05_reverse_and_07_flatten_a_list/res.ml
@@ -1,8 +1,17 @@
+(* https://github.com/ocaml/ocaml.org/blob/main/data/exercises/005_reverse_list.md *)
 (* https://github.com/ocaml/ocaml.org/blob/main/data/exercises/007_flatten_list.md *)
 
 type 'a node =
-  | One of 'a 
+  | One of 'a
   | Many of 'a node list
+
+
+let rev =
+  let rec go l' = function
+    | [] -> l'
+    | h::t -> rev (h::l') t
+  in go []
+
 
 let flatten t =
   let rec go cont t l =
@@ -10,4 +19,4 @@ let flatten t =
       One x :: t -> go cont t (x :: l)
     | Many l' :: t -> go (go cont t) l' l
     | [] -> cont l
-  in go List.rev t []
+  in go rev t []
