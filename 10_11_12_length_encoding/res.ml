f9ce2a02785a72999cb8f3ffc2ec4b56f446ccca
MV <m.viey@wanadoo.fr>
2024-03-19 13:44:03 +0100

    12: run-length encoding decode

--- 10_11_12_length_encoding/res.ml
+++ 10_11_12_length_encoding/res.ml
@@ -12,5 +12,13 @@ let encode l =
       if x = x' then (Many (2, x'))::t else (One x)::l'
   in
   List.fold_right process l []
+
+let decode l =
+  let process p l' =
+    match p with
+    | One x -> x::l'
+    | Many(n, x) ->
+      let s = Seq.repeat x |> Seq.take n in
+      Seq.fold_left (Fun.flip List.cons) l' s
   in
   List.fold_right process l []
